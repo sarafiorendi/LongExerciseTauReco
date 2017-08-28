6c338fb5610493c05914ab986b97f16e871d4705
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-08-28 22:44:12 +0200

    complete example

--- read_taus.py
+++ read_taus.py
@@ -1,5 +1,8 @@
+import ROOT
+from collections import OrderedDict
 from DataFormats.FWLite import Events, Handle
 from PhysicsTools.HeppyCore.utils.deltar import deltaR, deltaPhi
+from PhysicsTools.Heppy.physicsutils.TauDecayModes import tauDecayModes
 
 # Get ahold of the events
 events = Events('outputFULL.root')
@@ -26,6 +29,24 @@ def isGenHadTau(gen_tau):
     gen_tau.visphi = gen_tau.visp4.phi
     return isgentau
 
+# grab the final daughters (after any possible intermediate decay and radiative process)
+def finalDaughters(gen, daughters=None):
+    if daughters is None:
+        daughters = []
+    for i in range(gen.numberOfDaughters()):
+        daughter = gen.daughter(i)
+        if daughter.numberOfDaughters() == 0:
+            daughters.append(daughter)
+        else:
+            finalDaughters(daughter, daughters)
+    return daughters
+
+# example histogram
+histos = OrderedDict()
+histos['pull_pt'] = ROOT.TH1F('pull_pt', 'pull_pt', 50, -2, 2)
+histos['pull_pt'].GetXaxis().SetTitle('(p_{T}^{off} - p_{T}^{gen})/p_{T}^{gen}')
+histos['pull_pt'].GetYaxis().SetTitle('counts')
+
 # start looping on the events
 for i, ev in enumerate(events):
     
@@ -49,6 +70,11 @@ for i, ev in enumerate(events):
     # select only hadronically decaying taus
     gen_taus = [pp for pp in gen_particles if abs(pp.pdgId())==15 and pp.status()==2 and isGenHadTau(pp)]
 
+    # determine gen decaymode
+    for gg in gen_taus:
+        gg.decayMode = tauDecayModes.genDecayModeInt([d for d in finalDaughters(gg) \
+                                                      if abs(d.pdgId()) not in [12, 14, 16]])
+
     # match reco taus to gen taus
     for tt in taus    : tt.gen_tau  = None
     for gg in gen_taus: gg.reco_tau = None
@@ -64,21 +90,28 @@ for i, ev in enumerate(events):
         tt.gen_tau = bestmatch
         bestmatch.reco_tau = tt
         gen_taus_copy = [gg for gg in gen_taus_copy if gg != bestmatch]
+
+    # fill histograms
+    for gg in gen_taus:
+        if hasattr(gg, 'reco_tau') and gg.reco_tau:
+            pull = (gg.reco_tau.pt() - gg.vispt())/gg.vispt()
+            histos['pull_pt'].Fill(pull)
+
     
 
-    if len(gen_taus)>0 and len(taus)>0:
-        import pdb ; pdb.set_trace()
-        print '\n\n===========> RECO TAUS'
-        for tt in taus: 
-            print 'reco tau         pt %.3f eta %.3f phi %.3f' %(tt.pt(), tt.eta(), tt.phi())
-            if hasattr(tt, 'gen_tau') and tt.gen_tau:
-                print 'matched gen tau  pt %.3f eta %.3f phi %.3f' %(tt.gen_tau.vispt(), tt.gen_tau.viseta(), tt.gen_tau.visphi())
-            else:
-                print 'matched gen tau  MISSING'
-        print '===========> GEN TAUS'
-        for tt in gen_taus: 
-            print 'gen tau          pt %.3f eta %.3f phi %.3f' %(tt.vispt(), tt.viseta(), tt.visphi())
-            if hasattr(tt, 'reco_tau') and tt.reco_tau:
-                print 'matched reco tau pt %.3f eta %.3f phi %.3f' %(tt.reco_tau.pt(), tt.reco_tau.eta(), tt.reco_tau.phi())
-            else:
-                print 'matched reco tau MISSING'
+#     if len(gen_taus)>0 and len(taus)>0:
+#         import pdb ; pdb.set_trace()
+#         print '\n\n===========> RECO TAUS'
+#         for tt in taus: 
+#             print 'reco tau         pt %.3f eta %.3f phi %.3f' %(tt.pt(), tt.eta(), tt.phi())
+#             if hasattr(tt, 'gen_tau') and tt.gen_tau:
+#                 print 'matched gen tau  pt %.3f eta %.3f phi %.3f' %(tt.gen_tau.vispt(), tt.gen_tau.viseta(), tt.gen_tau.visphi())
+#             else:
+#                 print 'matched gen tau  MISSING'
+#         print '===========> GEN TAUS'
+#         for tt in gen_taus: 
+#             print 'gen tau          pt %.3f eta %.3f phi %.3f' %(tt.vispt(), tt.viseta(), tt.visphi())
+#             if hasattr(tt, 'reco_tau') and tt.reco_tau:
+#                 print 'matched reco tau pt %.3f eta %.3f phi %.3f' %(tt.reco_tau.pt(), tt.reco_tau.eta(), tt.reco_tau.phi())
+#             else:
+#                 print 'matched reco tau MISSING'
