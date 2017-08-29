5ffc63b4a40ee6b261c1b92801e79925afe0ff81
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-08-29 15:00:22 +0200

    add customisation example

---
+++ customise_tau_reco_miniaod_cfg.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+# trick to make python know where to look for the imports
+import sys
+sys.path.append('..')
+
+# for example: here
+from tau_miniaod import process
+
+# change the isolation cone
+process.combinatoricRecoTaus.isolationConeSize = cms.double(0.8) # originally 0.5
+
+# change the output file name, don't overwrite the original file!
+process.output.fileName = cms.untracked.string('outputFULL_isoCone0p8.root')
