80120d948f33774e1a67c41d29b944885ee58514
Janek <janek.bechtel@student.kit.edu>
2019-09-05 16:26:28 +0200

    Use TCanvas for plotting

--- plotting.py
+++ plotting.py
@@ -17,7 +17,7 @@ sample = args.file
 
 
 # open the input file and get the tree 
-infile = ROOT.TFile.Open('tau_gentau_tuple_{}.root'.format(sample), 'read')
+infile = ROOT.TFile.Open('tau_{}_tuple_{}.root'.format("jet" if sample=="QCD" else "gentau", sample), 'read')
 infile.cd()
 tree = infile.Get('tree')
 
@@ -29,11 +29,14 @@ histo_pull.GetYaxis().SetTitle('counts')
 # draw the histogram
 # require that the generator level pt to be larger than 18 GeV and that 
 # a good reconstructed tau is properly matched to the generator level tau
-tree.Draw('(tau_reco_pt - tau_gen_vis_pt)/tau_gen_vis_pt >> pull', 'tau_gen_vis_pt>18 & tau_reco_pt>0')
-
-# save the current pad in pdf format
-ROOT.gPad.SaveAs('pull_{}.pdf'.format(sample))
-ROOT.gPad.SaveAs('pull_{}.png'.format(sample))
+tree.Draw('(tau_reco_pt - tau_gen_vis_pt)/tau_gen_vis_pt >> pull', 'tau_gen_vis_pt>18 & abs(tau_gen_vis_eta)<2.4 & tau_reco_pt>0')
+c = ROOT.TCanvas("c","c")
+c.cd()
+histo_pull.Draw()
+# save the current canvas
+c.SaveAs('pull_{}.pdf'.format(sample))
+c.SaveAs('pull_{}.png'.format(sample))
+c.Clear()
 
 
 # efficiency example using TEfficiency https://root.cern.ch/doc/master/classTEfficiency.html
@@ -46,12 +49,14 @@ histo_num = ROOT.TH1F('num', 'num', len(bins)-1, bins)
 # fall in the detector acceptance (pt > 18 GeV and abs(eta)<2.4, tracker coverage)
 tree.Draw('tau_gen_vis_pt >> den', 'tau_gen_vis_pt>18 & abs(tau_gen_vis_eta)<2.4')
 tree.Draw('tau_gen_vis_pt >> num', 'tau_gen_vis_pt>18 & abs(tau_gen_vis_eta)<2.4 & tau_reco_pt>0')
-
+c = ROOT.TCanvas("c","c")
+c.cd()
 eff = ROOT.TEfficiency(histo_num, histo_den)
 eff.SetTitle(';generator-level #tau p_{T}^{vis} [GeV]; reconstruction efficiency')
 eff.SetMarkerStyle(8)
 eff.Draw('APL')
 
-# save the current pad in pdf format
-ROOT.gPad.SaveAs('reco_efficiency_{}.pdf'.format(sample))
-ROOT.gPad.SaveAs('reco_efficiency_{}.png'.format(sample))
+# save the current canvas
+c.SaveAs('reco_efficiency_{}.pdf'.format(sample))
+c.SaveAs('reco_efficiency_{}.png'.format(sample))
+c.Clear()
